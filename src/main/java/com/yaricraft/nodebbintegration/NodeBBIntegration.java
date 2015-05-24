e296c8794dfa56f1d4d788d990d264d243d37091
Timothy Fike <tafike@gmail.com>
2015-05-23 22:05:18 -0400

    Plugin base

---
+++ src/main/java/com/yaricraft/nodebbintegration/NodeBBIntegration.java
@@ -0,0 +1,9 @@
+package com.yaricraft.nodebbintegration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Yari on 5/23/2015.
+ */
+public class NodeBBIntegration extends JavaPlugin {
+}
