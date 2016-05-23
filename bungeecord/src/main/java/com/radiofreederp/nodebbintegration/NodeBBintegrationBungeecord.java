3d93aae1a958ed342bdafcb462e3084e9e539562
Timothy Fike <tafike@gmail.com>
2016-05-23 14:21:47 -0400

    add bungeecord template to universal jar

---
+++ bungeecord/src/main/java/com/radiofreederp/nodebbintegration/NodeBBintegrationBungeecord.java
@@ -0,0 +1,16 @@
+package com.radiofreederp.nodebbintegration;
+
+/**
+ * Created by Yari on 5/30/2016.
+ */
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class NodeBBintegrationBungeeCord extends Plugin {
+    @Override
+    public void onEnable() {
+        // You should not put an enable message in your plugin.
+        // BungeeCord already does so
+        getLogger().info("Yay! It loads!");
+    }
+}
